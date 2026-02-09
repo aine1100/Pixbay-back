218c8487a176eb0f7429694e53eb766cb845fb7d
aine1100 <ainedushimire@gmail.com>
2026-02-09 19:36:06 +0200

    feat: Add CORS middleware to the Express application to enable cross-origin requests.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth/authRoutes.js";
 import userRoutes from "./routes/user/userRoutes.js";
@@ -26,6 +27,7 @@ initSocket(server);
 setupSwagger(app);
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(globalRateLimiter);
 
